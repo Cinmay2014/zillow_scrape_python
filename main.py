0a9111e486f38c4889ab63ee2384dff3f2ee7780
Cinmay2014 <zhuyidong1120@gmail.com>
2023-04-29 18:53:31 -0400

    changed many codes, like using loop to get urls and datalist, add filters to beds# and baths#, store cities in a list  which can let user receive emails from multiple cities they interested in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pandas import DataFrame
 import datetime
 import os
-#os.chdir('./')
 import requests
 import re
 import json
@@ -9,22 +8,16 @@ import pandas as pd
 import smtplib
 import time
 import warnings
+from tabulate import tabulate
 warnings.filterwarnings('ignore')
-def sendinfo():
-    city = 'malden/' 
+def sendinfo(city_name):
+    city = f'{city_name}/'
+    #just grabbing the first 10 pages
+
+    base_url = 'https://www.zillow.com/homes/for_sale/'
+    urls = [f'{base_url}{city}/{page}_p/' for page in range(1, 11)]
+
     
-    #just grabbing the first 20 pages
-    #feel free to make this prettier
-    url1 = 'https://www.zillow.com/homes/for_sale/'+city
-    url2 = 'https://www.zillow.com/homes/for_sale/'+city+'/2_p/'
-    url3 = 'https://www.zillow.com/homes/for_sale/'+city+'/3_p/'
-    url4 = 'https://www.zillow.com/homes/for_sale/'+city+'/4_p/'
-    url5 = 'https://www.zillow.com/homes/for_sale/'+city+'/5_p/'
-    url6 = 'https://www.zillow.com/homes/for_sale/'+city+'/6_p/'
-    url7 = 'https://www.zillow.com/homes/for_sale/'+city+'/7_p/'
-    url8 = 'https://www.zillow.com/homes/for_sale/'+city+'/8_p/'
-    url9 = 'https://www.zillow.com/homes/for_sale/'+city+'/9_p/'
-    url10 = 'https://www.zillow.com/homes/for_sale/'+city+'/10_p/'
     
     #add headers in case you use chromedriver (captchas are no fun); namely used for chromedriver
     req_headers = {
@@ -34,38 +27,25 @@ def sendinfo():
         'upgrade-insecure-requests': '1',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     }
-    
     with requests.Session() as s:
-        r1 = s.get(url1, headers=req_headers)
-        r2 = s.get(url2, headers=req_headers)
-        r3 = s.get(url3, headers=req_headers)
-        r4 = s.get(url4, headers=req_headers)
-        r5 = s.get(url5, headers=req_headers)
-        r6 = s.get(url6, headers=req_headers)
-        r7 = s.get(url7, headers=req_headers)
-        r8 = s.get(url8, headers=req_headers)
-        r9 = s.get(url9, headers=req_headers)
-        r10 = s.get(url10, headers=req_headers)
-    
-        data1 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r1.text).group(1))
-        data2 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r2.text).group(1))
-        data3 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r3.text).group(1))
-        data4 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r4.text).group(1))
-        data5 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r5.text).group(1))
-        data6 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r6.text).group(1))
-        data7 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r7.text).group(1))
-        data8 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r8.text).group(1))
-        data9 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r9.text).group(1))
-        data10 = json.loads(re.search(r'!--(\{"queryState".*?)-->', r10.text).group(1))
-    
-    data_list = [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]
-                # ,data11,data12,data13,data14,data15,data16,data17,data18,data19,data20]
+        data_list = []
+        for url in urls:
+            response = s.get(url, headers=req_headers)
+            data = json.loads(re.search(r'!--(\{"queryState".*?)-->', response.text).group(1))
+            data_list.append(data)
+
+ 
     
     df = pd.DataFrame()
+    
+    
+    
     def make_frame(frame):
         for i in data_list:
             for item in i['cat1']['searchResults']['listResults']:
-                frame = frame.append(item, ignore_index=True)
+                ## Filter to only include listings with at least 3 bedrooms and at least 2 bathrooms
+                if item['beds'] is not None and item['beds'] >= 3 and item['baths'] is not None and item['baths'] >= 2:
+                    frame = frame.append(item, ignore_index=True)
         return frame
     df = make_frame(df)
         
@@ -79,33 +59,29 @@ def sendinfo():
     df['zestimate'] = df['zestimate'].fillna(0)
     df['best_deal'] = df['unformattedPrice'] - df['zestimate']
     
+     # Sort the DataFrame by the 'beds' column
+    #df = df.sort_values(by='beds', ascending=True)
+    
     #df['price per unit area'] = df['unformattedPrice'] / df['area']
     df = df.sort_values(by='best_deal',ascending=True)
     
-    print('shape:', df.shape)
-    #display(df[['id','address','beds','baths','area','price','zestimate','best_deal','hdpData']].head(20))
-    #
-    data = df[['statusText','address','beds','baths','area','price','brokerName','detailUrl']].head(40)
-    
+    #print('shape:', df.shape)
+    #display(df[['id','address','beds','baths','area','price','zestimate','best_deal','hdpData','brokerName',]].head(20))
+
+    data = df[['statusText','address','beds','baths','area','price','zestimate','best_deal','detailUrl']].head(30)
+    print(tabulate(data, headers='keys', tablefmt='psql', showindex=False))
     
     gmail_user = '@gmail.com' # Your email account
     gmail_password = '' # Your email password
     sent_from = gmail_user
     to = ['@gmail.com'] # target email account
-    # subject = "%s Malden House Price"%datetime.datetime.now().strftime('%Y%m%d %H')
-    # body = 'Result \n' + data.to_string()
-    # email_text = """\
-    # From: %s
-    # To: %s
-    # Subject: %s
-    # %s
-    # """ % (sent_from, ", ".join(to), subject, body)
-    
+   
     from email.mime.text import MIMEText
     from email.mime.application import MIMEApplication
     from email.mime.multipart import MIMEMultipart
     msg = MIMEMultipart()
-    msg['Subject'] = "%s Malden House Price"%datetime.datetime.now().strftime('%Y%m%d %H')
+    msg['Subject'] = f"{datetime.datetime.now().strftime('%Y%m%d %H:%M:%S')} {city_name}"
+
     
     html = """\
     <html>
@@ -129,16 +105,28 @@ def sendinfo():
     except Exception as ex:
         print ("Something went wrong….",ex)
     
-def timeSend():
-    startTime = datetime.datetime.now()
-    if startTime.hour == 10 and startTime.minute <= 1: # which means 10:00 am send you the email
-        sendinfo() # You can directly use this function to test if you can send the email.
-        time.sleep(80)
+    
+def time_send():
+    last_sent_time = None
+    while True:
+        current_time = datetime.datetime.now()
+        
+        # Check if the last email was sent 10 hours ago or hasn't been sent yet
+        if (last_sent_time is None) or (current_time - last_sent_time).seconds == 10*60*60:
+           
+            for city in cities:
+                print(city)
+                sendinfo(city)  # You can directly use this function to test if you can send the email.
+                
+            last_sent_time = datetime.datetime.now()  # Update the last sent time
+            time.sleep(5)  # Wait for 5 seconds before checking again
+           
+        else:
+            time.sleep(60)  # Wait for 1 minute before checking the time again
 
-while True:
-    timeSend()
-    time.sleep(10)
 
-
+# You can modify it to your cities
+cities = ['malden','weston','andover','belmont']
+time_send()
 
 
